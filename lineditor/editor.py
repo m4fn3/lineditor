5afc906670c4d36ebc1c103200f0cb42fcd0cc02
mafusuke <mafusukee@gmail.com>
2020-12-20 22:47:03 +0900

    Implemented read_only mode and added type annotations

--- lineditor/editor.py
+++ lineditor/editor.py
@@ -1,45 +1,58 @@
 import os
 
 class Editor:
-    def __init__(self, path: str, create=True, auto_save=True, encoding="utf-8"):
+    def __init__(self, path: str, create=True, auto_save=True, read_only=False, encoding="utf-8"):
+
+        self.auto_save: bool = auto_save
+        self.path: str = path
+        self.encoding: str = encoding
+        self.read_only: bool = read_only
+        self.cached = []
+
         if not os.path.isfile(path):
             if create:
                 with open(path, "w", encoding=encoding):
                     pass
             else:
                 raise RuntimeError("File doesn't exists at '" + path + "'")
-        self.auto_save = auto_save
-        self.path = path
-        self.encoding = encoding
-        with open(path, "r", encoding=encoding) as f:
-            self.cached = f.readlines()
 
-    def save(self):
+        self._cache()
+
+    def save(self) -> None:
         self._save()
 
-    def __getitem__(self, index):
+    def reload(self) -> None:
+        self._cache()
+
+    def __getitem__(self, index: int) -> str:
         if not (0 < int(index) <= len(self.cached)):
             raise RuntimeError("Line number out of range")
         return self._read(index)
 
-    def __setitem__(self, index, text):
+    def __setitem__(self, index: int, text: str) -> None:
         if not (0 < index <= len(self.cached)):
             raise RuntimeError("Line number out of range")
+        elif self.read_only:
+            raise RuntimeError("File loaded with read-only mode")
         self._write(index, text)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.cached)
 
-    def _write(self, line, text):
+    def _cache(self):
+        with open(self.path, "r", encoding=self.encoding) as f:
+            self.cached = f.readlines()
+
+    def _write(self, line: int, text: str) -> None:
         self.cached[line-1] = text + "\n"
         if self.auto_save:
             self._save()
 
-    def _read(self, line):
-        return self.cached[line-1]
+    def _read(self, line: int) -> str:
+        return self.cached[line-1].rstrip("\n")
 
-    def _save(self):
-        with open(self.path, "r+", encoding=self.encoding) as f:
+    def _save(self) -> None:
+        with open(self.path, "w", encoding=self.encoding) as f:
             f.writelines(self.cached)
 
 
